7460ee7993eec59d8868d3afb02b2b3f92ab5c7e
Ahmed Obad <amo4oma@users.noreply.github.com>
2020-12-09 03:24:17 +0800

    final update
    final update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-
-
+// fiching the data from the Api provided
 const url = "https://worldtimeapi.org/api/timezone/Asia/Kuala_Lumpur";
 
 fetch(url)
-.then(response => response.json())
-.then(data => desplay(data)).catch(error => {
+  .then(response => response.json())
+  .then(data => desplay(data)).catch(error => {
     alert("API erorr please refrish");
   });;
 
-function desplay(name){
-  $("h1").text(name.datetime.slice(11 , 19));
-  $("h2").text(name.datetime.slice(0 , 10));
+// updating the h1 & h2 with the time from the Api
+
+function desplay(name) {
+  $("h1").text(name.datetime.slice(11, 19));
+  $("h2").text(name.datetime.slice(0, 10));
 }
