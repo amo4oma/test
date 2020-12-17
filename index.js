0a46ee3b399bc308cb97f7b09167dd81e6822bc1
Ahmed Obad <amo4oma@users.noreply.github.com>
2020-12-17 20:10:15 +0800

    Solveinhere
    for review

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // fiching the data from the Api provided
-const url = "https://worldtimeapi.org/api/timezone/Asia/Kuala_Lumpur";
+// const url = "https://worldtimeapi.org/api/timezone/Asia/Kuala_Lumpur"; // Since I am Using HTTPS, the API url has to be HTTPS to work. if its HTTP the request may be blocked
+//
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => desplay(data)).catch(error => {
+//     // alert("API erorr please refrish");
+//   });;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => desplay(data)).catch(error => {
-    alert("API erorr please refrish");
-  });;
-
+$(".about").css("color","#2E2E2E");
 // updating the h1 & h2 with the time from the Api
 
-function desplay(name) {
-  $("h1").text(name.datetime.slice(11, 19));
-  $("h2").text(name.datetime.slice(0, 10));
-}
+// function desplay(name) {
+//   $("h1").text(name.datetime.slice(11, 19));
+//   $("h2").text(name.datetime.slice(0, 10));
+// }
